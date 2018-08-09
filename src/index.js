2e435e6c3fa59d0bac4a55db0beb1ffc8c7d5357
Efren Garcia Home <efren.gl@gmail.com>
2018-08-08 22:44:40 -0500

    config antd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Hello from './Hello';
+import ReactDOM from 'react-dom';
+import { LocaleProvider, DatePicker, message } from 'antd';
+// The default locale is en-US, but we can change it to other language
+import frFR from 'antd/lib/locale-provider/es_ES';
+import moment from 'moment';
+import 'moment/locale/es';
+
+moment.locale('es');
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: ''
+    };
+  }
+  handleChange(date) {
+    message.info('Selected Date: ' + (date ? date.toString() : ''));
+    this.setState({ date });
+  }
   render() {
     return (
-      <div>
-        <Hello hello={'Hello, world! And the people of the world!.....'} />
-      </div>
+      <LocaleProvider locale={frFR}>
+        <div style={{ width: 400, margin: '100px auto' }}>
+          <DatePicker onChange={value => this.handleChange(value)} />
+          <div style={{ marginTop: 20 }}>
+            Date: {this.state.date && this.state.date.toString()}
+          </div>
+        </div>
+      </LocaleProvider>
     );
   }
 }
-render(<App />, document.getElementById('app'));
+
+ReactDOM.render(<App />, document.getElementById('app'));
